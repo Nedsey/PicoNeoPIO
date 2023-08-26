25ecf0b49181ba31fa5e33438dbdffeeccd7aa05
Nayben <58311871+Nedsey@users.noreply.github.com>
2023-08-26 03:26:59 -0500

    Create blink.h

---
+++ blink.h
@@ -0,0 +1,33 @@
+#ifndef NEOPIXEL_DRIVER_H
+#define NEOPIXEL_DRIVER_H
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "signal_gen.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int redColor;
+    int blueColor;
+    int greenColor;
+} Pixel;
+
+extern int pixelCount;
+extern Pixel *arrayPixels;
+extern uint pin;
+extern PIO pio;
+extern uint offset;
+extern uint sm;
+
+void declares();
+void initPixels(int GPIO, int pixelCounts);
+int convColor(int red, int blue, int green);
+void setPixel(int pixel, int red, int green, int blue);
+void fillPixels(int red, int green, int blue);
+void resetPixels();
+int randColor();
+int getPixelValue(int pixel, char value[]);
+
+#endif /* NEOPIXEL_DRIVER_H */
